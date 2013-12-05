dc6528995a99c0e4c93ac0df61b502607eb22c1a
cresencio <krescruz@gmail.com>
2013-12-04 18:48:20 -0600

    Delete tracer-ng.js

--- app/app/apps/demo/static/js/controllers/tracer-ng.js
+++
@@ -1,6 +0,0 @@
-var tracerApp = angular.module('appDemo',[]);
-
-tracerApp.config(function($interpolateProvider) {
-    $interpolateProvider.startSymbol('{$');
-    $interpolateProvider.endSymbol('$}');
-});
